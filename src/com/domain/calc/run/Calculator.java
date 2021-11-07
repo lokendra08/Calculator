b090828203cac1f0bfe270250f8c108b6d0556e6
techcreation85 <60753576+techcreation85@users.noreply.github.com>
2021-11-07 19:45:05 +0530

    code formatting

--- src/com/domain/calc/run/Calculator.java
+++ src/com/domain/calc/run/Calculator.java
@@ -17,12 +17,13 @@ import com.domain.calc.validation.InfixValidationServiceImpl;
 
 /**
  * Main program to run calculator
+ * 
  * @author Lokendra Rawat
  * @version 1.0
- * @since   2021-11-07
+ * @since 2021-11-07
  */
 public class Calculator {
-	
+
 	static {
 		CalcDisplayBanner.displayInfo();
 	}
@@ -30,60 +31,67 @@ public class Calculator {
 	public static void main(String[] args) {
 		run();
 	}
-	
-   /**
-	* Takes user input from console as an string and invoke calculator service.
-	* this method also remove white spaces if any present in the input expression.
-	* this method also add 0 as prefix  to the use input to handle infix expression starting with minus sign
-	* 
-	* @return void
-	* @exception CalculatorOperandException, CalculatorValidatorException
-	*/
-	private static void run(){
-		String input;
-		try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){		
-    	do {
-    		CalculatorUtils.lineBreaks(1);
-    		System.out.println("Enter arithmatic expression Or Enter 'exit' to terminate the program : "); 
-    		CalculatorUtils.lineBreaks(1);
-            input = reader.readLine();;
-            if(input.equals(CalculatorConstants.EXIT)) {
-    			break;
-    		}
-    		input = CalculatorUtils.removeWhiteSpaces(input, CalculatorConstants.NO_SPACE);  
-    		input = CalculatorUtils.addZeroAsPrefix(input, CalculatorConstants.ZERO);
-    		try {
-				invokeCalculatorService(input, new Stack<Double>(), new Stack<Character>(), new InfixValidationServiceImpl());
-    		}catch(CalculatorValidatorException cvex) {
-    			System.out.println(cvex.getMessage());
-				System.out.println(CalculatorConstants.ERROR_MESSAGE);
-    		}catch(CalculatorOperandException coex) {
-				System.out.println(coex.getMessage());
-				System.out.println(CalculatorConstants.ERROR_MESSAGE);
-			} catch (Exception ex) {
-				System.out.println(CalculatorConstants.ERROR_MESSAGE);
-			} 
-    	}while(true);
-	  } catch (IOException e) {
-		e.printStackTrace();
-	}
-   }
 
-	
-   /**
-	* Takes user input from console as an string and invoke calculator service.
-	* this method also remove white spaces if any present in the input expression.
-	* this method also add 0 as prefix  to the use input to handle infix expression starting with minus sign
-	* 
-	* @param input   user input expression as infix notation
-	* @param values  stack to hold operands
-	* @param operators stack to hold operators
-	* @param validationService  validation service for respective calculation service
-	* @return void
-	* @exception CalculatorOperandException, CalculatorValidatorException
-	*/
-	private static void invokeCalculatorService(String input, Stack<Double> values, Stack<Character> operators, IValidationService validationService) throws CalculatorOperandException, CalculatorValidatorException {
-		ICalculationService calculatorService = ObjectCreationFactory.getObjectfactory(CalculatorConstants.CALCULATION_SERVICE).getCalculationService(CalculatorConstants.INFIX, values, operators, validationService);
-		System.out.println("Answer: "+calculatorService.evaluate(input));
+	/**
+	 * Takes user input from console as an string and invoke calculator service.
+	 * this method also remove white spaces if any present in the input expression.
+	 * this method also add 0 as prefix to the use input to handle infix expression
+	 * starting with minus sign
+	 * 
+	 * @return void
+	 * @exception CalculatorOperandException, CalculatorValidatorException
+	 */
+	private static void run() {
+		String input;
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			do {
+				CalculatorUtils.lineBreaks(1);
+				System.out.println("Enter arithmatic expression Or Enter 'exit' to terminate the program : ");
+				CalculatorUtils.lineBreaks(1);
+				input = reader.readLine();
+				;
+				if (input.equals(CalculatorConstants.EXIT)) {
+					break;
+				}
+				input = CalculatorUtils.removeWhiteSpaces(input, CalculatorConstants.NO_SPACE);
+				input = CalculatorUtils.addZeroAsPrefix(input, CalculatorConstants.ZERO);
+				try {
+					invokeCalculatorService(input, new Stack<Double>(), new Stack<Character>(),
+							new InfixValidationServiceImpl());
+				} catch (CalculatorValidatorException cvex) {
+					System.out.println(cvex.getMessage());
+					System.out.println(CalculatorConstants.ERROR_MESSAGE);
+				} catch (CalculatorOperandException coex) {
+					System.out.println(coex.getMessage());
+					System.out.println(CalculatorConstants.ERROR_MESSAGE);
+				} catch (Exception ex) {
+					System.out.println(CalculatorConstants.ERROR_MESSAGE);
+				}
+			} while (true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Takes user input from console as an string and invoke calculator service.
+	 * this method also remove white spaces if any present in the input expression.
+	 * this method also add 0 as prefix to the use input to handle infix expression
+	 * starting with minus sign
+	 * 
+	 * @param input             user input expression as infix notation
+	 * @param values            stack to hold operands
+	 * @param operators         stack to hold operators
+	 * @param validationService validation service for respective calculation
+	 *                          service
+	 * @return void
+	 * @exception CalculatorOperandException, CalculatorValidatorException
+	 */
+	private static void invokeCalculatorService(String input, Stack<Double> values, Stack<Character> operators,
+			IValidationService validationService) throws CalculatorOperandException, CalculatorValidatorException {
+		ICalculationService calculatorService = ObjectCreationFactory
+				.getObjectfactory(CalculatorConstants.CALCULATION_SERVICE)
+				.getCalculationService(CalculatorConstants.INFIX, values, operators, validationService);
+		System.out.println("Answer: " + calculatorService.evaluate(input));
 	}
 }
